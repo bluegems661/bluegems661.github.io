44efb2766409eef2769152f3981cc11d8af7e98b
Your Name <your.email@example.com>
2026-04-27 17:36:50 +0200

    Configure custom domain maxzinke.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://bluegems661.github.io',
+  site: 'https://maxzinke.com',
   trailingSlash: 'never',
   build: {
     inlineStylesheets: 'auto',
